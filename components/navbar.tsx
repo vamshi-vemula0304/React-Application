50579fecbfc26d513a610a46b67cc3fd4d8351c4
vamshi-vemula0304 <vamshi.vemula4087@gmail.com>
2023-07-11 15:15:24 +0530

    changed anchor taged to link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,33 +1,42 @@
-import styles from '../styles/navbar.module.css'
+import Link from 'next/link';
+import styles from '../styles/navbar.module.css';
 
 export default function NavBar() {
-    return <div className={ styles.header }>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                {/* <a className="navbar-brand" href="#"></a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">HOME</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/todo">TODO</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/shop">SHOP</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" href='/weather'>WEATHER APP</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/about">ABOUT</a>
-                        </li>
-                    </ul>
+    return (
+        <div className={styles.header}>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" href="/" >
+                                    HOME
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" href="/todo" >
+                                    TODO
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" href="/shop" >
+                                    SHOP
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" href="/weather" >
+                                    WEATHER APP
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link active" href="/about" >
+                                    ABOUT
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </div>
+            </nav>
+        </div>
+    );
 }
